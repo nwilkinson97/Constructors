1757a1919bcb245b799d01d5b970ff17e615063a
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-21 14:25:41 -0500

    Add Customer Class
    Adding a Customer Class.

--- Constructors/Program.cs
+++ Constructors/Program.cs
@@ -1,5 +1,10 @@
 ﻿namespace Constructors
 {
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+    }
     class Program
     {
         static void Main(string[] args)
