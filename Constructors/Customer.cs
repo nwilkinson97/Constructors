9d27d4b2a30f2813e668711f95583108f3be854d
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-21 14:28:28 -0500

    Quick Actions
    Click on the Name of the Class "Customer" use the Quick Actions
    Keyboard Shortcut.   CTRL} + .      and select the Option to "Move type to "Customer.cs"    This will move the Customer class to a new file.

---
+++ Constructors/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace Constructors
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+    }
+}
--- Constructors/Program.cs
+++ Constructors/Program.cs
@@ -1,10 +1,5 @@
 ﻿namespace Constructors
 {
-    public class Customer
-    {
-        public int Id;
-        public string Name;
-    }
     class Program
     {
         static void Main(string[] args)
