74b0c554c00963d7cf318eb7029eab9d5bfd96bb
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-21 14:36:12 -0500

    Added Comments
    Added comments to the customer class showing how to use Keyboard shortcuts to open the code window from the Solutions Explorer.  {CTRL} +{ALT} + L  and {F7} to pen the code widow of selected file.

--- Constructors/Program.cs
+++ Constructors/Program.cs
@@ -1,5 +1,10 @@
 ﻿namespace Constructors
 {
+    // After having used the Quick Actions to move the Customer Class to a new file
+    // you can use the keyboard shortcut to open the Solution Explorer {CTRL} + {ALT} + L
+    // that will place the focus in the solutions explorer.  You can then highlight the
+    // Customer.cs and Press {F7} on the keyboard.  The {F7} shorcut will open the Code
+    // Window with the Customer.cs contents.
     class Program
     {
         static void Main(string[] args)
