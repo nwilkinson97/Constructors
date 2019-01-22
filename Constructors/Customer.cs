33a770daee919dcbf5912903a21274cf36213575
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-22 11:15:49 -0500

    Comments Added
    Comments discussing Alternative Solution and Readability

--- Constructors/Customer.cs
+++ Constructors/Customer.cs
@@ -10,7 +10,11 @@ namespace Constructors
 
         // Here you will notice when you first add a List it is not recognized as a valid type.
         // to resolve this use the Quick Actions shortcut {CTRL} + . and add the appropriate 
-        // using statement.
+        // using statement.  
+
+        // NOTE:  You could initialize the Orders Field here.  Instead of using inheritance.  However,
+        //    -- for this lessone I wanted to show how we can use the this keyword to inherit or link
+        //     --> our class constructors.
         public List<Order> Orders;
 
         /// <summary>
@@ -24,6 +28,8 @@ namespace Constructors
 
         public Customer(int id): this()
         {
+            // Note for readability use the this keyword.  Also using this keyword will access the list of fields
+            // or properties avaialble and hide the integer 'id' value passed.
             this.Id = id;
         }
 
