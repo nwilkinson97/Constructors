f5e008d8da26a22a77bd3198551c04fc8c08e894
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-22 08:58:27 -0500

    Default Constructors
    Adding a default constructor.  Note here if we go back to the Program.cs the Cusotmer class has a red squiggly line.  Just like from MS Word when you have misspelled a word however, in this case it indicates that we are not using the correct syntax.
    
    Note:  Here we do not have a constructor that accepts no arguments.  In order to fix we will need to assign the appropriate arguement(s)

--- Constructors/Customer.cs
+++ Constructors/Customer.cs
@@ -5,5 +5,20 @@ namespace Constructors
     {
         public int Id;
         public string Name;
+
+        /// <summary>
+        /// Default Constructor for the customer class. 
+        /// Note:  Code Snippet ctor {TAB}
+        /// </summary>
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
     }
 }
